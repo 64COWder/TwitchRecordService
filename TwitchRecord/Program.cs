a1d7626a62e3e25017b8f5a31850ba19671d5816
James Bouwens <potatoesroll@gmail.com>
2016-06-05 11:16:54 -0400

    Initial commit

---
+++ TwitchRecord/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using AutoTwitchRecord;
+using Newtonsoft.Json;
+
+namespace TwitchRecord
+{
+    public class Program
+    {
+        public static readonly string ChannelName = ConfigurationManager.AppSettings["ChannelName"];
+        public static readonly string SaveToFolder = ConfigurationManager.AppSettings["SaveToFolder"];
+
+        public static void Main(string[] args)
+        {
+            StreamerIsLive();
+            if (!IsLivestreamerRunning())
+            {
+                if (StreamerIsLive())
+                {
+                    StartLivestreamerRecording();
+                }
+            }
+        }
+
+        private static void StartLivestreamerRecording()
+        {
+            var command = @"C:\Program Files (x86)\Livestreamer\livestreamer.exe";
+            var args = $@"twitch.tv/{ChannelName} best -o {SaveToFolder}" + ChannelName + "-" + DateTime.Now.ToString("yyyyMMdd-hhmmss") + ".mp4";
+
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = command,
+                    Arguments = args
+                }
+            };
+            process.Start();
+        }
+
+        private static bool StreamerIsLive()
+        {
+            using (var webClient = new WebClient())
+            {
+                var stream = JsonConvert.DeserializeObject<RootObject>(webClient.DownloadString($"https://api.twitch.tv/kraken/streams/{ChannelName}"));
+                return stream?.stream != null;
+            }
+        }
+
+        public static bool IsLivestreamerRunning()
+        {
+            return Process.GetProcessesByName("livestreamer").Any();
+        }
+    }
+}
---
+++ TwitchRecordService/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System.ServiceProcess;
+using System.Timers;
+
+namespace TwitchRecordService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer timer;
+
+        public Service1()
+        {
+            ServiceName = "TwitchRecord";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer(30000D); // 30 seconds
+            timer.AutoReset = true;
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+            timer = null;
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            TwitchRecord.Program.Main(null);
+        }
+    }
+}
